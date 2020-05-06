48e145b1f0eda7faa1bba7cd24eb56210dc4da29
Yuttasak (Yo) Pannawat <yuttasakyo@gmail.com>
2020-05-06 13:15:21 +0700

    Create taskDTO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,11 +12,18 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getById(@Param('id') id: string): Task {
+    return this.tasksService.getById(id);
+  }
+
   @Post()
-  create(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.tasksService.create(title, description);
+  create(@Body() createTaskDTO: CreateTaskDTO): Task {
+    return this.tasksService.create(createTaskDTO);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string): string {
+    return this.tasksService.delete(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuidv1 } from 'uuid';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,18 @@ export class TasksService {
     return this.tasks;
   }
 
-  create(title: string, description: string): Task {
+  getById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  delete(id: string): string {
+    const indexOfTask = this.tasks.findIndex(task => task.id === id);
+    this.tasks.splice(indexOfTask, 1);
+    return 'OK';
+  }
+
+  create(createTaskDTO: CreateTaskDTO): Task {
+    const { title, description } = createTaskDTO;
     const task: Task = {
       id: uuidv1(),
       title,
